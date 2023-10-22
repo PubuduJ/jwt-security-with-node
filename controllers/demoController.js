fb18483c42c2513fee500bffc8ed2b56aeb20099
Pubudu Janith <pubudujanith123@gmail.com>
2023-10-22 15:37:27 +0530

    Update demoController logics

--- controllers/demoController.js
+++ controllers/demoController.js
@@ -1,6 +1,7 @@
 const {StatusCodes} = require("http-status-codes");
 
 const sayHello = (req, res) => {
+    console.log(req.user);
     return res.status(StatusCodes.OK).send("Hello from secured end point");
 }
 
